ec075999516597e5928968cec9516ccef7c40ec5
Marcus <marcus@hardt-it.de>
2019-11-09 20:36:23 +0100

    improve installation

--- install.sh
+++ install.sh
@@ -13,22 +13,46 @@ test -z $PIP && {
     }
 }
 
+echo "pip: ${PIP}"
 
 
-test -d dist && {
-    echo "You should remove the dist dir first"
-    exit 1
-}
+# test -d dist && {
+#     echo "You should remove the dist dir first"
+#     exit 1
+# }
+
 python3 setup.py sdist 
-${PIP} install dist/${SERVICE}*tar.gz
+FULLNAME=`python3 setup.py --fullname`
 
-# test -e ${LIB_SYSD}/${SERVICE}.service || {
-    # /usr/bin/passwordd install
-    # passwordd install
-test -e ${LIB_SYSD}/${SERVICE}.service && {
-    rm -f ${LIB_SYSD}/${SERVICE}.service
+echo -e "\nDone building ${FULLNAME}\n"
+
+# IF we're root, we go and install
+[ ${UID} == 0 ] && {
+    ${PIP} install dist/${FULLNAME}*tar.gz
+    test -e ${LIB_SYSD}/${SERVICE}.service && {
+        diff -q systemd/mqtt-to-influx.service ${LIB_SYSD} >/dev/null || {
+            rm -f ${LIB_SYSD}/${SERVICE}.service
+        }
+    }
+    test -e ${LIB_SYSD}/${SERVICE}.service || {
+        cp systemd/mqtt-to-influx.service ${LIB_SYSD}
+    }
+    systemctl enable $SERVICE
+    systemctl restart $SERVICE
 }
-cp systemd/mqtt-to-influx.service ${LIB_SYSD}
 
-systemctl enable $SERVICE
-systemctl restart $SERVICE
+# else we print the necessary steps
+[ ${UID} == 0 ] || {
+    echo -e "\nPlease run the following commands as root:\n"
+
+    echo "    ${PIP} install dist/${FULLNAME}*tar.gz"
+    test -e ${LIB_SYSD}/${SERVICE}.service && {
+        diff -q systemd/mqtt-to-influx.service ${LIB_SYSD} >/dev/null || {
+            echo "    rm -f ${LIB_SYSD}/${SERVICE}.service"
+            echo "    cp systemd/mqtt-to-influx.service ${LIB_SYSD}"
+        }
+    }
+    echo "    systemctl enable $SERVICE"
+    echo "    systemctl restart $SERVICE"
+    echo -e "\nto install the software as a daemon"
+}
