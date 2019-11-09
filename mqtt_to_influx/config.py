d861cab77c7c64c7900532f320f11024764ec87a
Marcus <marcus@hardt-it.de>
2019-11-09 17:40:35 +0100

    change config file locations

--- mqtt_to_influx/config.py
+++ mqtt_to_influx/config.py
@@ -10,8 +10,10 @@ CONFIG.optionxform = lambda option: option
 def reload():
     """Reload configuration from disk.
 
-    Config locations, by priority:
+    Config locations, by priority (all values are merged. The last one
+        overwrites earlier ones)
     $MQTT_TO_INFLUX_CONFIG
+    /etc/mqtt-to-influx/mqtt-to-influx.pathconf
     ./mqtt-to-influx.pathconf
     ~/.config/mqtt-to-influx/mqtt-to-influx.pathconf
     """
@@ -22,9 +24,9 @@ def reload():
         files += [Path(filename)]
 
     files += [
-        Path('./mqtt-to-influx.pathconf'),
+        Path('/')/'etc'/'mqtt-to-influx'/'mqtt-to-influx.pathconf',
         Path.home()/'.config'/'mqtt-to-influx'/'mqtt-to-influx.pathconf',
-        Path('/')/'etc'/'mqtt-to-influx.pathconf'
+        Path('./mqtt-to-influx.pathconf')
     ]
 
     CONFIG.read(files)
